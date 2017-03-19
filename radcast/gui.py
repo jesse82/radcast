548c8db4efd2834257d2146eea2dc620183eb267
Josh Wheeler <mantlepro@gmail.com>
2017-03-18 23:31:42 -0700

    An almost working prototype

--- radcast/gui.py
+++ radcast/gui.py
@@ -3,6 +3,11 @@
 """gui.py: graphical user interface to radcast"""
 
 from Tkinter import *
+from tkFileDialog import askopenfilename
+from radcast import logging
+import mlt
+import os
+import yaml
 
 __copyright__ = "Copyright 2007, Josh Wheeler"
 __license__ = "GPL"
@@ -24,31 +29,266 @@ __status__ = "Development"
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+filename = "Choose a file"
 
-class InfoFrame(Frame):
+
+class Settings:
+
+    """Project settings, and preferences"""
+
+    def __init__(self):
+
+        try:  # if current directory exists
+            self.project_dir = os.getcwd()
+        except OSError:  # if not, go back a directory and try again
+            os.chdir("..")
+            self.project_dir = os.getcwd()
+
+        # set initial values
+        self.title = "UNTITLED"
+        self.profile = "atsc_720p_2997"
+        self.media_dir = None
+        self.frame_rate = 30
+        self.drop_frame = True
+        self.jog_skip_frames = 8
+        self.still_seconds = 6
+        self.transition_length = int(self.frame_rate)
+        self.pre_roll = int(self.frame_rate)
+
+        try:  # if settings file exists in current dir, read into cfg
+
+            with open(self.project_dir + "/settings.yml", 'r') as ymlfile:
+                cfg = yaml.safe_load(ymlfile)
+
+                # TODO deserialize implicitly instead of explicitly
+
+                self.title = cfg.get('title', self.title)
+                self.project_dir = cfg.get('project_dir', self.project_dir)
+                self.media_dir = cfg.get('media_dir', self.media_dir)
+                self.profile = cfg.get('profile', self.profile)
+                self.frame_rate = cfg.get('frame_rate', self.frame_rate)
+                self.pre_roll = int(cfg.get('pre_roll', self.pre_roll))
+                self.drop_frame = cfg.get('drop_frame', self.drop_frame)
+                self.jog_skip_frames = cfg.get('jog_skip_frames', self.jog_skip_frames)
+                self.still_seconds = cfg.get('still_seconds', self.still_seconds)
+                self.transition_length = int(cfg.get('transition_length', self.transition_length))
+
+        except yaml.YAMLError, e:
+
+            if hasattr(e, 'problem_mark'):
+                mark = e.problem_mark
+                print("Error in settings file: (line %s, column %s)" % (
+                    mark.line + 1, mark.column + 1)
+                )
+
+        except:
+
+            print "Something went wrong in the settings"
+
+        # set dependent variables
+        self.still_frames = self.still_seconds * self.frame_rate
+        self.mlt_profile = mlt.Profile(self.profile)
+
+settings = Settings()
+
+
+class Player:
+
+    """MLT player https://www.mltframework.org/docs/framework/"""
+
+    def __init__(self):
+
+        self.current_clip = "NULL"
+
+        # initialize mlt
+        mlt.Factory().init()
+
+        # initialize producer with NULL if no current_clip is available
+        self.producer = mlt.Producer(settings.mlt_profile, self.current_clip)
+
+        # initialize and start consumer
+        self.consumer = mlt.Consumer(settings.mlt_profile, "sdl")
+        self.consumer.set("rescale", "bicubic")
+        self.consumer.set("resize", 1)
+        self.consumer.set("real_time", 1)
+        self.consumer.set("progressive", 1)
+        self.connect_producer()
+
+    def connect_producer(self):
+        self.consumer.purge()
+        self.producer.set_speed(0)
+        self.consumer.connect(self.producer)
+        #self.consumer.start()
+
+    def stop(self):
+        self.producer.set_speed(0)
+
+    def pause(self):
+        self.producer.set_speed(0)
+
+    def play(self):
+        self.producer.set_speed(1)
+
+    def reverse(self):
+        self.producer.set_speed(-1)
+
+    def shuttle_forward(self):
+        speed = self.producer.get_speed()
+        if speed < 1:
+            self.producer.set_speed(1)
+        elif speed == 1:
+            self.producer.set_speed(2)
+        elif speed == 2:
+            self.producer.set_speed(5)
+        elif speed == 5:
+            self.producer.set_speed(10)
+
+    def shuttle_reverse(self):
+        speed = self.producer.get_speed()
+        if speed >= 0:
+            self.producer.set_speed(-1)
+        elif speed == -1:
+            self.producer.set_speed(-2)
+        elif speed == -2:
+            self.producer.set_speed(-5)
+        elif speed == -5:
+            self.producer.set_speed(-10)
+
+    def jog(self, amount):
+        """Jog forward or reverse by negative or positive amount"""
+        # pause playback if not paused already
+        self.producer.set_speed(0)
+        frame = self.producer.frame()
+        self.producer.seek(frame + amount)
+
+    def get_frame(self):
+        return self.producer.frame()
+
+    def length(self):
+        return self.producer.get_length()
+
+    def seek_frame(self, frame):
+        length = self.length()
+        if frame < 0:
+            frame = 0
+        elif frame >= length:
+            frame = length - 1
+        self.producer.set_speed(0)
+        self.producer.seek(frame)
+
+    def end(self):
+        length = self.length()
+        last_frame = length - 1
+        self.seek_frame(last_frame)
+
+    def load_file(self, path):
+        self.producer = mlt.Producer(settings.mlt_profile, path)
+        self.connect_producer()
+        self.current_clip = path
+
+    def is_stopped(self):
+        if self.producer.get_speed() == 0:
+            return True
+
+    def toggle_play_pause(self):
+        if self.is_stopped():
+            self.producer.set_speed(1)
+        else:
+            self.producer.set_speed(0)
+
+player = Player()
+
+
+class MainFrame(Frame):
     """Collect info needed for podcast"""
 
+    def keypress(self, event):
+
+        key = event.keysym
+        if key == 'Left':
+            player.jog(-1)
+        if key == 'Right':
+            player.jog(+1)
+        if event == repr('<Shift-Right>'):
+            player.jog(+10)
+        if key == 'i':
+            self.set_in()
+        if key == 'o':
+            self.set_out()
+        if key == 'space':
+            player.toggle_play_pause()
+        if key == 'Home':
+            player.seek_frame(0)
+        if key == 'End':
+            player.seek_frame(player.length())
+        print repr(event.keysym)
+
     def __init__(self, parent):
-
+        global filename
         Frame.__init__(self, parent)
+        root.bind_class("Text", "<Control-a>", self.selectall)
+        root.bind("<Key>", self.keypress)
+        self.in_frame = 0
+        self.out_frame = 0
 
-        self.info_frame = Frame(root)
-        self.info_frame.pack(padx=20, pady=20)
-
+        # create inner frames
+        self.player_frame = Frame(root)
+        self.main_frame = Frame(root)
         self.button_frame = Frame(root)
+
+        # pack frames
+        self.player_frame.pack()
+        self.main_frame.pack()
         self.button_frame.pack()
 
+        # validation goodness
+        vcmd = parent.register(self.validate)
+
+        if filename:
+            file_label = Label(self.player_frame, text="File: %s" % filename)
+
+            self.in_button = Button(self.player_frame, text="Set IN", command=self.set_in)
+            self.in_label = Label(self.player_frame, text="(0)")
+
+            self.out_button = Button(self.player_frame, text="Set OUT", command=self.set_out)
+            self.out_label = Label(self.player_frame, text="(0)")
+
+            player.load_file(filename)
+
+            self.preview_in_button = Button(self.player_frame, text="Preview IN")
+            self.preview_out_button = Button(self.player_frame, text="Preview OUT")
+
+            file_label.pack(side=TOP)
+
+            self.in_button.pack(side=LEFT)
+            self.out_button.pack(side=RIGHT)
+
+            self.in_label.pack(side=LEFT)
+            self.out_label.pack(side=RIGHT)
+
+            self.preview_in_button.pack(side=LEFT)
+            self.preview_out_button.pack(side=RIGHT)
+
+        else:
+            app.open_file()
+
         # title
-        Label(self.info_frame, text="Title", anchor=W).grid(row=0, sticky=W)
-        self.title = Entry(self.info_frame, width=70)
-        self.title.grid(row=1, pady=(4,10), sticky=W)
+        Label(self.main_frame, text="Title", anchor=W).grid(row=1, sticky=W)
+        self.title = Entry(self.main_frame, width=70, validate='key', validatecommand=(vcmd, '%P'))
+        self.title.grid(row=2, pady=(4, 10), sticky=W)
 
         # description
-        Label(self.info_frame, text="Description", anchor=W).grid(row=3, sticky=W)
-        self.description = Text(self.info_frame, height=7, wrap=WORD)
+        Label(self.main_frame, text="Description", anchor=W).grid(row=3, sticky=W)
+        self.description = Text(self.main_frame, height=7, wrap=WORD)
+        self.description.bind("<KeyRelease>", self.validate)
         self.description.grid(row=4, pady=(4,10), sticky=W)
 
-        self.GO = Button(self.button_frame, text="Upload Podcast", command=self.go)
+        # buttons
+        self.GO = Button(self.button_frame,
+                         text="Upload Podcast",
+                         command=self.go,
+                         state=DISABLED)
+
         self.GO.pack({"side": "left"})
 
         self.QUIT = Button(self.button_frame)
@@ -56,15 +296,34 @@ class InfoFrame(Frame):
         self.QUIT["command"] = self.quit
         self.QUIT.pack({"side": "right"})
 
+    def set_in(self):
+        self.in_frame = player.get_frame()
+        self.in_label.config(text="(%s)" % self.in_frame)
+        print("Set in frame to %s" % self.in_frame)
+
+    def set_out(self):
+        self.out_frame = player.get_frame()
+        self.out_label.config(text="(%s)" % self.out_frame)
+        print("Set out frame %s" % self.out_frame)
+
+    def selectall(self, event):
+        event.widget.tag_add("sel","1.0","end")
+
+    def validate(self, P):
+        """Insure user input is as correct as this model can be"""
+        title = self.title.get().strip()
+        description = self.description.get(1.0, END).strip()
+        self.GO.config(state=(NORMAL if title and description else DISABLED))
+        return True
+
     def go(self):
-        # TODO Validate user input
+        """Run radcast commands"""
 
         title = self.title.get().strip()
         description = self.description.get(1.0, END).strip()
 
         if self.title and self.description:
-            print("%s\n%s" % (self.title, self.description))
-
+            print("%s\n%s" % (title, description))
 
 class Application(Frame):
     """Allows the user to set up podcast, choose an in/out point and upload"""
@@ -78,14 +337,8 @@ class Application(Frame):
         Frame.__init__(self, master)
         self.pack()
         self.create_menu()
-        self.create_widgets()
-        root.bind_class("Text", "<Control-a>", self.selectall)
-
-    def selectall(self, event):
-        event.widget.tag_add("sel","1.0","end")
-
-    def open_file(self):
-        pass
+        self.open_file()
+        self.player_frame()
 
     def about(self):
         pass
@@ -100,19 +353,51 @@ class Application(Frame):
         menu.add_cascade(label="File", menu=file_menu)
         file_menu.add_command(label="Open...", command=self.open_file)
         file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.quit)
+        file_menu.add_command(label="Exit", command=self.quit, accelerator='Ctrl+Q')
 
         help_menu = Menu(menu)
         menu.add_cascade(label="Help", menu=help_menu)
         help_menu.add_command(label="About...", command=self.about)
 
-    def create_widgets(self):
-        self.info_frame = InfoFrame(root)
+    def open_file(self):
+        """File chooser"""
+        global filename
 
+        FILETYPES = [
+            ('Movie files', ('.mov', '.mkv', '.avi', '.mp4', '.m4v', 'mpg', 'mpeg', 'mpv', 'mp2', 'm2v', 'mjpeg', 'webm', 'ogg', 'ogv', 'ogm')),
+        ]
+
+        try:
+            # filename = askopenfilename(filetypes=FILETYPES)
+            filename = "/tmp/a.mp4"
+            # move to status bar in a later version
+        except TypeError, e:
+            logging.error("Either the wrong filetype was chosen or no file was.")
+
+    def player_frame(self):
+        self.player_frame = Frame(root, width=650, height=400)
+        self.player_frame.pack()
+
+        # make a home for the SDL window
+        env = os.environ['SDL_WINDOWID'] = str(self.player_frame.winfo_id())
+        print env
+
+        # update root so consumer can connect
+        root.update()
+
+        # start the consumer in the SDL frame
+        player.consumer.start()
 
 
 
 root = Tk()
+
 app = Application(master=root)
+main_frame = MainFrame(root)
+
+
+# key bindings
+root.bind('<Control-q>', quit)
+
 app.mainloop()
 root.destroy()
